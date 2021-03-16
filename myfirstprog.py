0b234db92bc114783794c9a1ef730a75c0c54109
guljabinquadri <78896565+guljabinquadri@users.noreply.github.com>
2021-03-16 19:09:16 +0530

    Create myfirstprog.py

---
+++ myfirstprog.py
@@ -0,0 +1,9 @@
+print("Hello World 6")
+# hash is used in python for singel line comment
+""" triple quotes it used
+for multi line comment"""
+print("Hello guljabin this the first program in", "python.")
+print("next line") #write next line the line will print in next line
+# \n it is the comment for next line
+print("Guljabin \nQuadri\tis agood girl.") #\t it is the comment for tab in the line
+#goggle escape code in python
