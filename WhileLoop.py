726d42c7b7defff236fe4d6d658761e8f63987ff
Guljabin Quadri <78896565+guljabinquadri@users.noreply.github.com>
2021-03-20 22:20:46 +0530

    init

---
+++ WhileLoop.py
@@ -0,0 +1,12 @@
+#while loops
+
+i = 0    #initialization
+while(i<45): #finalization
+    print(i) #what it to be printed
+    i = 1 +i  #increment so that i won't be in infinte loop
+
+# if we want to print the numbers in a signal line then in while loop
+"""i = 0
+while(i<20):
+    print(i , end=" ")
+    i = i+1 """
