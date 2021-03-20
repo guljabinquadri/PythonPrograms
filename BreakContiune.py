198dc8fae9b0662ce24737844b78a1c503aa485e
Guljabin Quadri <78896565+guljabinquadri@users.noreply.github.com>
2021-03-20 22:21:45 +0530

    init

---
+++ BreakContiune.py
@@ -0,0 +1,14 @@
+
+
+j = 5
+while(True):#or while(1) #it will run the result continuosly
+    if j<=9:
+        j= j+1
+        continue #The continue keyword is used to end the current iteration
+                        # in a for loop (or a while loop), and continues to the next iteration.
+                         # (the condtion b4 continue will not print)
+    print(j, end=" ")
+    if(j==44):
+        break #stop the loop
+    j = j+1
+
