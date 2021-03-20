6c16ce3ee512e0a796e2012d93342e92f1f8377f
Guljabin Quadri <78896565+guljabinquadri@users.noreply.github.com>
2021-03-20 17:28:41 +0530

    init

---
+++ For_loops.py
@@ -0,0 +1,24 @@
+#For loops
+
+list1 = ["Harry", "Larry", "Marry"]
+for item in list1:
+    print(item)
+
+# For loop in list
+list2 = [ ["harry",18], ["Larry",19], ["Marry",21] ]
+for item in list2:
+    print(item)
+for item, age in list2:
+    print(item, "and their age is", age)
+
+#for in dictinoery
+dict1 = dict(list2)
+print(dict1)
+"""for item, age in dict1: # it will show error in the dict case
+    # but if we will write dict1.items() then it will work
+    print(item, age)"""
+for item, age in dict1.items():
+    print(item, "is", age)
+# for geeting only the key value then
+for item in dict1:
+    print(item)
