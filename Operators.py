44f40387f10318a2889a7bfddd3c38d79f9ca337
Guljabin Quadri <78896565+guljabinquadri@users.noreply.github.com>
2021-03-23 00:03:04 +0530

    init

--- Operators.py
+++ Operators.py
@@ -1,10 +1,40 @@
-#Assignment operators
-
+#Arthimatic Operators
 print(" 5 + 6 is", 5+6)
 print(" 5 - 6 is", 5-6)
+print(" 5 ** 6 is", 5**6) #exponancial power 5^6
 print(" 5 * 6 is", 5*6)
 print(" 5 / 6 is", 5/6)
 print(" 5 // 6 is", 5//6)
+print(" 5 % 6 is", 5%6) #modulus operatir which gives the remainder
 # // gives integer value by ignoring values after decimal point
 #flor operators
+
+#Assignment operators
+
+print("Assignment Operators")
+x = 5
+x +=4   #x+4
+print(x)
+x/=3    #x/3
+print(x)#Comparion Operators
+i = 5
+print(i==4)
+print(i<= 6)
+print(i>=5)
+print(i!=5)
+#Logical Operators
+a = True
+b = False
+print( a and b)
+#Identity Operators
+print(a is b)
+print( a is not b)
+#Membership Operators
+list =[3,32, 45, 56, 70,0.4]
+print(0.4 in list)
+print(3 not in list)
+#Bitwise Operators
+print(0 & 1)
+print( 1 | 0)
+
 
