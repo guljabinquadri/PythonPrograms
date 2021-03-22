27ebd6bdc3651fceb6293e2248154661ee30bafe
Guljabin Quadri <78896565+guljabinquadri@users.noreply.github.com>
2021-03-22 23:09:27 +0530

    init

---
+++ Operators.py
@@ -0,0 +1,10 @@
+#Assignment operators
+
+print(" 5 + 6 is", 5+6)
+print(" 5 - 6 is", 5-6)
+print(" 5 * 6 is", 5*6)
+print(" 5 / 6 is", 5/6)
+print(" 5 // 6 is", 5//6)
+# // gives integer value by ignoring values after decimal point
+#flor operators
+
