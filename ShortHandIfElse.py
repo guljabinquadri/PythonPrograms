f586c3c4210801690ff6276434568111436170c3
Guljabin Quadri <78896565+guljabinquadri@users.noreply.github.com>
2021-03-23 00:03:49 +0530

    init

---
+++ ShortHandIfElse.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a\n"))
+b = int(input("Enter b\n"))
+
+if a>b :print("A is greater")
+
+c = int(input("Enter c\n"))
+d = int(input("Enter d\n"))
+print("C is greater") if c>d else print("D is greater")
+# true condition false
